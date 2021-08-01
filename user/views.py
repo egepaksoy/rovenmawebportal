87855f656cca961eb1b8ecb4fe6e3ae2141db53f
egepaksoy <egepaskoy@gmail.com>
2021-08-01 14:35:43 +0300

    Aktif Kullanıcı için kendini silme koruması eklendi

--- user/views.py
+++ user/views.py
@@ -8,83 +8,89 @@ from django.contrib.auth.decorators import login_required
 
 
 def delete_user(request, username):
-  if request.method == "POST":
-    try:
-        user = User.objects.get(username=username)
-        user.delete()
-        print("kullanıcı silindi")
-        messages.success(request, "Kullanıcı Silindi: "+username)
+    if request.method == "POST" and request.user != username:
+        try:
+            user = User.objects.get(username=username)
+            user.delete()
+            print("kullanıcı silindi")
+            messages.success(request, "Kullanıcı Silindi: " + username)
 
-    except User.DoesNotExist:
-        messages.error(request, "Kullanıcı Bulunamadı")
-  return redirect('register')
+        except User.DoesNotExist:
+            messages.error(request, "Kullanıcı Bulunamadı")
+
+    return redirect('register')
 
 
 def register(request):
-  if request.user.is_authenticated:
+    if request.user.is_authenticated and request.user.is_superuser:
 
-    form = CreateUserForm()
+        form = CreateUserForm()
 
-    if request.method == "POST":
-      form = CreateUserForm(request.POST)
-      password1 = request.POST.get('password1')
-      password2 = request.POST.get('password2')
+        if request.method == "POST":
+            form = CreateUserForm(request.POST)
+            password1 = request.POST.get('password1')
+            password2 = request.POST.get('password2')
 
-      if form.is_valid():
-        form.save()
-        username = form.cleaned_data.get("username")
-        messages.success(request, "Kullanıcı Oluşturuldu: " + username)
-        usertype = request.POST.get("usertype")
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get("username")
+                messages.success(request, "Kullanıcı Oluşturuldu: " + username)
+                usertype = request.POST.get("usertype")
 
-        user = User.objects.get(username=username)
+                user = User.objects.get(username=username)
 
-        if usertype == "superuser":
-          user.is_staff = True
-          user.is_superuser = True
-          user.save()
-        elif usertype == "staff":
-          user.is_staff = True
-          user.is_superuser = False
-          user.save()
+                if usertype == "superuser":
+                    user.is_staff = True
+                    user.is_superuser = True
+                    user.save()
+                elif usertype == "staff":
+                    user.is_staff = True
+                    user.is_superuser = False
+                    user.save()
 
-        return redirect('register')
+                return redirect('register')
 
-      messages.info(request, form.errors)
+            messages.info(request, form.errors)
 
-    context = {
-      "form": form,
-      "users": User.objects.all(),
-      }
+        context = {
+            "form": form,
+            "users": User.objects.all(),
+        }
 
-    return render(request, "register.html", context)
-  else:
-    return redirect('login')
+        return render(request, "register.html", context)
+
+    if request.user.is_superuser == False:
+        messages.info(request, "Bu alana erişmek için Super Admin olmanız gerekir.")
+        return redirect("home")
+
+    else:
+        return redirect('login')
 
 
 def login_view(request):
-  if request.user.is_authenticated:
-    return redirect('home')
-  else:
-    if request.method == "POST":
-      username = request.POST.get("username")
-      password = request.POST.get("password")
-      user = authenticate(request, username=username, password=password)
-
-      if user is not None:
-        login(request, user)
+    if request.user.is_authenticated:
         return redirect('home')
+    else:
+        if request.method == "POST":
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            user = authenticate(request, username=username, password=password)
 
-      else:
-        messages.info(request, 'Kullanıcı Adı Veya Parola Hatalı')
+            if user is not None:
+                login(request, user)
+                return redirect('home')
 
-    return render(request, "login.html")
+            else:
+                messages.info(request, 'Kullanıcı Adı Veya Parola Hatalı')
+
+        return render(request, "login.html")
 
 
 def logout_view(request):
-  logout(request)
-  return redirect("login")
+    logout(request)
+    return redirect("login")
 
 
 @login_required(login_url="login")
 def home(request):
-  return render(request, "home.html")
+    return render(request, "home.html")
