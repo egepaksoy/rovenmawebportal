c0c92f4516bd19468434246467f9e6f1ce428fe1
egepaksoy <egepaskoy@gmail.com>
2021-08-01 13:50:58 +0300

    Kullanıcı Adı ve Soyadı da kaydediliyor

--- user/forms.py
+++ user/forms.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 
 
 class CreateUserForm(UserCreationForm):
+  first_name = forms.CharField(max_length=80, help_text="Kullanıcı Adı Soyadı")
+
   class Meta:
     model = User
-    fields = ['username', 'password1', 'password2']
+    fields = ["first_name", 'username', 'password1', 'password2']
--- user/views.py
+++ user/views.py
@@ -10,10 +10,10 @@ from django.contrib.auth.decorators import login_required
 def delete_user(request, username):
   if request.method == "POST":
     try:
-        user = User.objects.get(username = username)
+        user = User.objects.get(username=username)
         user.delete()
         print("kullanıcı silindi")
-        messages.success(request, "Kullanıcı Silindi")            
+        messages.success(request, "Kullanıcı Silindi: "+username)
 
     except User.DoesNotExist:
         messages.error(request, "Kullanıcı Bulunamadı")
@@ -22,18 +22,25 @@ def delete_user(request, username):
 
 def register(request):
   if request.user.is_authenticated:
-    
+
     form = CreateUserForm()
 
     if request.method == "POST":
       form = CreateUserForm(request.POST)
-      password1 = request.POST.get("password1")
-      if form.is_valid():
-        form.save() 
-        user = form.cleaned_data.get("username")
-        messages.success(request, "User created: " + user)
-        return redirect('home')
+      password1 = request.POST.get('password1')
+      password2 = request.POST.get('password2')
 
+      if form.is_valid():
+        form.save()
+        user = form.cleaned_data.get("username")
+        messages.success(request, "Kullanıcı Oluşturuldu: " + user)
+        return redirect('register')
+
+      elif password2 != password1:
+        messages.info(request, "Şifreler Birbirinden Farklı")
+
+      else:
+        messages.info(request, "Kullanıcı sisteme kayıtlı")
     context = {
       "form": form,
       "users": User.objects.all(),
@@ -51,14 +58,14 @@ def login_view(request):
     if request.method == "POST":
       username = request.POST.get("username")
       password = request.POST.get("password")
-      user = authenticate(request ,username=username, password=password)
-      
+      user = authenticate(request, username=username, password=password)
+
       if user is not None:
         login(request, user)
         return redirect('home')
-      
+
       else:
-        messages.info(request, 'Incorrect username or password')
+        messages.info(request, 'Kullanıcı Adı Veya Parola Hatalı')
 
     return render(request, "login.html")
 
