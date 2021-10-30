a0f086ef022200333db713bceba6797e317c26f0
Ege Paksoy <egepaskoy@gmail.com>
2021-10-30 22:40:35 +0300

    articles eklendi

--- page_articles/models.py
+++ page_articles/models.py
@@ -187,6 +187,11 @@ class StaticsArticle(models.Model):
         verbose_name="Number of last packs (text)", max_length=40)
     error_text = models.CharField(
         verbose_name="Your Browser Does Not Support HTML5 (text)", max_length=50, default=None)
+    free = models.CharField(verbose_name="Free", max_length=10, null=True, default=None)
+    now = models.CharField(verbose_name="Now", max_length=10, null=True, default=None)
+    memory = models.CharField(verbose_name="Memory", max_length=15, null=True, default=None)
+    ssd = models.CharField(verbose_name="SSD", max_length=15, null=True, default=None)
+    cpu = models.CharField(verbose_name="CPU", max_length=15, null=True, default=None)
 
     def __str__(self):
         return self.lang
